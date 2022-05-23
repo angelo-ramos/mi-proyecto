41782d61ca61d11f8b6ae68640cb09d615e9591f
Angelo Ramos <20192258@pucp.edu.pe>
2022-05-23 11:05:19 -0500

    clase animal y gato creadas

---
+++ src/com/company/Animal.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Animal {
+    private String raza;
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+}
---
+++ src/com/company/Gato.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Gato extends Animal{
+    private int alturaSalto;
+    private int numeroVida = 7;
+
+    public int getAlturaSalto() {
+        return alturaSalto;
+    }
+
+    public void setAlturaSalto(int alturaSalto) {
+        this.alturaSalto = alturaSalto;
+    }
+
+    public int getNumeroVida() {
+        return numeroVida;
+    }
+
+    public void setNumeroVida(int numeroVida) {
+        this.numeroVida = numeroVida;
+    }
+}

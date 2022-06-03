49d756e9c5a5ba41244beee52921ca9ddb16f2a9
Miguel <a20190050@pucp.edu.pe>
2022-06-02 22:06:48 -0500

    edicion

--- src/com/company/Perro.java
+++ src/com/company/Perro.java
@@ -2,6 +2,15 @@ package com.company;
 
 public class Perro extends Animal{
     private String modeloCorrea = "Perro pituco";
+    private String casa;
+
+    public String getCasa() {
+        return casa;
+    }
+
+    public void setCasa(String casa) {
+        this.casa = casa;
+    }
 
     public String getModeloCorrea() {
         return modeloCorrea;
